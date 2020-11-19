db635e8608a4e872e7a7978db16e014da295da03
EC2 Default User <ec2-user@ip-172-31-5-154.us-east-2.compute.internal>
2020-11-19 04:37:31 +0000

    add category

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  get "posts/new" => "posts#new"
-  get "posts/index" => "posts#index"
-  get "posts/:id" => "posts#show"
-  get "posts/:id/edit" => "posts#edit"
-  post "posts/:id/destroy" => "posts#destroy"
-  post "posts/:id/comments/create" => "comments#create"
-  post "posts/create"=>"posts#create"
-  root "home#top"
-  get "/about" => "home#about"
-  post "users/create" => "users#create"
-  get "signup" => "users#new"
-  get "users/:id" => "users#show"
-  get "/login" => "users#login_form"
-  post "/login" => "users#login"
-  post "logout" => "users#logout"
-  post "users/:id/update" => "users#update"
-  get "users/:id/edit" => "users#edit"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ root "home#top"
+ get "/about", to: "home#about"
+ get    '/login',   to: 'sessions#new'
+ post   '/login',   to: 'sessions#create'
+ delete '/logout',  to: 'sessions#destroy'
+ resources :users
+ resources :posts do
+   resources :comments
+ end
 end
