9914127feddf21e264acf1b254c76a9786bb1588
EC2 Default User <ec2-user@ip-172-31-5-154.us-east-2.compute.internal>
2021-01-15 08:04:45 +0000

    addanonymous

--- app/assets/javascripts/comment_count.js
+++ app/assets/javascripts/comment_count.js
@@ -1,6 +1,5 @@
 /*global $*/
 
-// jquery書きはじめ
 $(function (){
   // 処理（ページが読み込まれた時、フォームに残り何文字入力できるかを数えて表示する）
 
@@ -8,25 +7,25 @@ $(function (){
   //\nは"改行"に変換して2文字にする。オプションフラグgで文字列の最後まで\nを探し変換する
   var count = $(".js-text").text().replace(/\n/g, "改行").length;
   //残りの入力できる文字数を計算
-  var now_count = 1000 - count;
+  var now_count = 300 - count;
   //文字数がオーバーしていたら文字色を赤にする
-  if (count > 1000) {
-    $(".js-text-count").css("color","red");
+  if (count > 300) {
+    $(".comment-count").css("color","red");
   }
   //残りの入力できる文字数を表示
-  $(".js-text-count").text( "あと" + now_count + "文字");
+  $(".js-comment-count").text( "あと" + now_count + "文字");
 
-  $(".js-text").on("keyup", function() {
+  $(".js-comment").on("keyup", function() {
     // 処理（キーボードを押した時、フォームに残り何文字入力できるかを数えて表示する）
     //フォームのvalueの文字数を数える
     var count = $(this).val().replace(/\n/g, "改行").length;
-    var now_count = 1000 - count;
+    var now_count = 300 - count;
 
-    if (count > 1000) {
-      $(".js-text-count").css("color","red");
+    if (count > 300) {
+      $(".js-comment-count").css("color","red");
     } else {
-      $(".js-text-count").css("color","green");
+      $(".js-comment-count").css("color","green");
     }
-    $(".js-text-count").text( "あと" + now_count + "文字");
+    $(".js-comment-count").text( "あと" + now_count + "文字");
   });
 });
